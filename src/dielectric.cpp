25cefc5ebc38262e106c2ed3935b5765310228c1
Justian <just.tzh@outlook.com>
2022-08-13 20:17:29 +0800

    完成作业4以及作业5的一小部分

--- src/dielectric.cpp
+++ src/dielectric.cpp
@@ -9,42 +9,122 @@
 
 NORI_NAMESPACE_BEGIN
 
+//
+    static Vector3f refract(const Vector3f& viewDir, const Vector3f& normal, float eta)
+    {
+        //判断光线在物体内部还是外部
+        bool isOuter = normal.dot(viewDir) > 0;
+        auto N = isOuter ? normal : -normal;
+        eta = isOuter ? eta : (1.0f / eta);
+
+        float cosThetaI = viewDir.dot(N);
+        float cosThetaT2 = 1 - eta * eta * (1 - cosThetaI * cosThetaI);
+
+        if (cosThetaT2 <= 0)
+        {
+            return Vector3f(0.0f);
+        }
+        float cosThetaT = sqrt(cosThetaT2);
+        return -viewDir * eta + N * (cosThetaI * eta - cosThetaT);
+    }
+
 /// Ideal dielectric BSDF
-class Dielectric : public BSDF {
-public:
-    Dielectric(const PropertyList &propList) {
-        /* Interior IOR (default: BK7 borosilicate optical glass) */
-        m_intIOR = propList.getFloat("intIOR", 1.5046f);
+    class Dielectric : public BSDF
+    {
+    public:
+        Dielectric(const PropertyList& propList)
+        {
+            /* Interior IOR (default: BK7 borosilicate optical glass) */
+            m_intIOR = propList.getFloat("intIOR", 1.5046f);
 
-        /* Exterior IOR (default: air) */
-        m_extIOR = propList.getFloat("extIOR", 1.000277f);
-    }
+            /* Exterior IOR (default: air) */
+            m_extIOR = propList.getFloat("extIOR", 1.000277f);
+        }
 
-    Color3f eval(const BSDFQueryRecord &) const {
-        /* Discrete BRDFs always evaluate to zero in Nori */
-        return Color3f(0.0f);
-    }
+        Color3f eval(const BSDFQueryRecord&) const
+        {
+            /* Discrete BRDFs always evaluate to zero in Nori */
+            return Color3f(0.0f);
+        }
 
-    float pdf(const BSDFQueryRecord &) const {
-        /* Discrete BRDFs always evaluate to zero in Nori */
-        return 0.0f;
-    }
+        float pdf(const BSDFQueryRecord&) const
+        {
+            /* Discrete BRDFs always evaluate to zero in Nori */
+            return 0.0f;
+        }
 
-    Color3f sample(BSDFQueryRecord &bRec, const Point2f &sample) const {
-        throw NoriException("Unimplemented!");
-    }
+        Color3f sample(BSDFQueryRecord& bRec, const Point2f& sample) const
+        {//throw NoriException("Unimplemented!");
+            Vector3f normalLocal{0., 0., 1.};
+            float cosThetaI = Frame::cosTheta(bRec.wi);
+            bool entering = (cosThetaI > 0.f);
+            float etaI, etaT;
+            if (entering)
+            {
+                etaI = m_extIOR;
+                etaT = m_intIOR;
+            }
+            else
+            {
+                etaI = m_intIOR;
+                etaT = m_extIOR;
+                normalLocal = -normalLocal;
+                cosThetaI = -cosThetaI;
+            }
+            bRec.eta = etaI / etaT;
+            float sin2ThetaI = Frame::sinTheta2(bRec.wi);
+            float sin2ThetaT = bRec.eta * bRec.eta * sin2ThetaI;
+            if (sin2ThetaT >= 1.)
+            { //全反射
+                bRec.wo = Vector3f{-bRec.wi.x(), -bRec.wi.y(), bRec.wi.z()};
+            }
+            else
+            {
+                //反射的比例，精确
+                float fresnelTerm = nori::fresnel(cosThetaI, etaI, etaT);
+                //反射的比例，近似
+                //float fre =  SchlicksFresnel(cosThetaI, etaI, etaT);
+                if (sample.x() < fresnelTerm)
+                {
+                    bRec.wo = Vector3f{-bRec.wi.x(), -bRec.wi.y(), bRec.wi.z()};
+                }
+                else
+                {
+                    float cosThetaT = std::sqrt(1. - sin2ThetaT);
+                    //球坐标方法
 
-    std::string toString() const {
-        return tfm::format(
-            "Dielectric[\n"
-            "  intIOR = %f,\n"
-            "  extIOR = %f\n"
-            "]",
-            m_intIOR, m_extIOR);
-    }
-private:
-    float m_intIOR, m_extIOR;
-};
+                    float thetaT;
+                    if (entering)
+                    {
+                        thetaT = M_PI - acos(cosThetaT);
+                    }
+                    else
+                    {
+                        thetaT = acos(cosThetaT);
+                    }
+                    float phiT = std::atan2(bRec.wi.y(), bRec.wi.x()) + M_PI;
+                    bRec.wo = sphericalDirection(thetaT, phiT);
 
-NORI_REGISTER_CLASS(Dielectric, "dielectric");
+                    //向量方法
+                    //bRec.wo = bRec.eta * -bRec.wi + (bRec.eta * cosThetaI - cosThetaT) * normalLocal;
+                }
+            }
+            return Color3f(1.);
+        }
+
+        std::string toString() const
+        {
+            return tfm::format(
+                    "Dielectric[\n"
+                    "  intIOR = %f,\n"
+                    "  extIOR = %f\n"
+                    "]",
+                    m_intIOR, m_extIOR);
+        }
+
+    private:
+        float m_intIOR, m_extIOR;
+    };
+
+    NORI_REGISTER_CLASS(Dielectric, "dielectric");
 NORI_NAMESPACE_END
--- src/whitted.cpp
+++ src/whitted.cpp
@@ -7,37 +7,76 @@
 NORI_NAMESPACE_BEGIN
     class WhittedIntegrator : public Integrator
     {
+    private:
+        int maxDepth = 10;
     public:
         WhittedIntegrator(const PropertyList& props) {}
 
-        Color3f Li(const Scene* scene, Sampler* sampler, const Ray3f& ray) const
+        Color3f Li(const Scene* scene, Sampler* sampler, const Ray3f& ray_) const
         {
-            Intersection its;
-            Color3f color(0.0f);
-            //判断击中场景中物体
-            if(!scene->rayIntersect(ray,its)){
-                return color;
+            int depth = 0;
+            Color3f radiance(0.0f);
+            float coff = 1.0f;
+            Ray3f ray = ray_;
+            while (depth < maxDepth)
+            {
+                depth++;
+                Intersection its;
+                //判断击中场景中物体
+                if (!scene->rayIntersect(ray, its))
+                {
+                    continue;
+                }
+                Color3f Le(0.0f);
+                //直接击中光源
+                if (its.mesh->isEmitter())
+                {
+                    EmitterQueryRecord record(ray.o, its.p, its.shFrame.n);
+                    Le = its.mesh->getEmitter()->eval(record);
+                }
+                //漫反射不再弹射
+                if (its.mesh->getBSDF()->isDiffuse())
+                {
+                    //均匀选取光源上一点
+                    Mesh* light = scene->getRandomEmitter(sampler->next1D());
+                    EmitterQueryRecord directRecord(its.p);
+                    //对光源采样
+                    Color3f Li = light->getEmitter()->sample(light, directRecord, sampler);
+
+                    //阴影测试
+                    if (scene->rayIntersect(directRecord.shadowRay))
+                    {
+                        Li = 0;
+                    }
+
+                    //在着色点采样BxDF
+                    float cosTheta = Frame::cosTheta(its.shFrame.toLocal(directRecord.wi));
+                    BSDFQueryRecord bsdf(its.toLocal(-ray.d), its.toLocal(directRecord.wi), ESolidAngle);
+                    Color3f f = its.mesh->getBSDF()->eval(bsdf);
+                    if (cosTheta < 0)
+                    {
+                        cosTheta = 0;
+                    }
+                    radiance += Le + Li * f * cosTheta / (1.0f / (float) scene->getEmitters().size());
+                    break;
+                }
+                else
+                {
+                    //在着色点采样BxDF
+                    BSDFQueryRecord bRec(its.toLocal(-ray.d));
+                    Color3f refColor = its.mesh->getBSDF()->sample(bRec, sampler->next2D());//采样一个出射方向
+
+                    //俄罗斯轮盘赌
+                    if (sampler->next1D() > 0.95 && refColor.x() <= 0.f)
+                    {
+                        break;
+                    }
+
+                    ray = Ray3f(its.p, its.toWorld(bRec.wo));
+                    coff /= 0.95f;
+                }
             }
-            Color3f Le(0.0f);
-            //直接击中光源
-            if(its.mesh->isEmitter()){
-                EmitterQueryRecord record(ray.o,its.p,its.shFrame.n);
-                Le = its.mesh->getEmitter()->eval(record);
-            }
-            //未击中则计算直接光照
-            Mesh* light = scene->getRandomEmitter(sampler->next1D());
-            EmitterQueryRecord directRecord(its.p);
-            Color3f Li = light->getEmitter()->sample(light,directRecord,sampler);
-            if(scene->rayIntersect(directRecord.shadowRay)){
-                Li = 0;
-            }
-            float cosTheta = Frame::cosTheta(its.shFrame.toLocal(directRecord.wi));
-            BSDFQueryRecord bsdf(its.toLocal(-ray.d),its.toLocal(directRecord.wi),ESolidAngle);
-            Color3f f = its.mesh->getBSDF()->eval(bsdf);
-            if(cosTheta<0){
-                cosTheta = 0;
-            }
-            return Le+Li*f*cosTheta/(1.0f/(float)scene->getEmitters().size());
+            return radiance *= coff;
         }
 
         std::string toString() const
